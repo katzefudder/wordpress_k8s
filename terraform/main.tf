bf4c77747b9f92261c7040d11397e32bc8f58990
Florian Dehn <flo@katzefudder.de>
2022-12-14 17:41:57 +0100

    change order

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,16 @@
-module "ingress" {
-  source = "./modules/ingress"
-}
-
 module "monitoring" {
   depends_on = [module.ingress]
   source = "./modules/monitoring"
 }
 
+module "ingress" {
+  source = "./modules/ingress"
+}
+
 module "wordpress" {
+  depends_on = [
+    module.monitoring
+  ]
   source = "./modules/wordpress"
   stage = var.stage
 }
