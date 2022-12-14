402749269333c1b6f519de11e6dfbb74a35cf6cc
Florian Dehn <flo@katzefudder.de>
2022-12-14 17:41:40 +0100

    add a session cookie

--- terraform/modules/wordpress/ingress.tf
+++ terraform/modules/wordpress/ingress.tf
@@ -4,12 +4,16 @@ resource "kubernetes_ingress_v1" "wordpress" {
       namespace = kubernetes_namespace.wordpress.metadata.0.name
       annotations = {
         "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+        "nginx.ingress.kubernetes.io/affinity" = "cookie"
+        "nginx.ingress.kubernetes.io/session-cookie-name" = "sticky-session"
+        "nginx.ingress.kubernetes.io/session-cookie-expires" = "172800"
+        "nginx.ingress.kubernetes.io/session-cookie-max-age" = "172800"
       }
    }
    spec {
     ingress_class_name = "nginx"
       rule {
-        host = "wordpress.localhost"
+        host = "azure.katzefudder.de"
         http {
          path {
            path = "/"
